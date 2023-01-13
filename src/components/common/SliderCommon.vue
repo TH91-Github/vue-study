bf47924b99942ea4cbe2c664951b67fd5374d17e
TH91-Github <je8035@ipartners.co.kr>
2023-01-13 16:56:44 +0900

    230113 슬라이드 수정 누락

--- src/components/common/SliderCommon.vue
+++ src/components/common/SliderCommon.vue
@@ -5,12 +5,28 @@
     v-bind="swiperOpt"
     @swiper="onSwiper"
     >
+    <!-- 슬라이드 값 있을 시-->
+    <template v-if="slideList.length>0">
       <SwiperSlide 
         v-for="swiper in slideList"
         :key="swiper.name">
-          {{ swiper }}
+        <SliderItem>
+          <slot v-bind="swiper"></slot>
+        </SliderItem>
       </SwiperSlide>
+    </template>
+    <!-- 슬라이드 값 없을 시 -->
     
+    <template v-else>
+      <!-- 기본 값 슬라이드 2 제공 : 테스트용 -->
+      <SwiperSlide
+        v-for="n in 3"
+        :key="n">
+        <SliderItem>
+          <slot></slot>
+        </SliderItem>
+      </SwiperSlide>
+    </template>
   </swiper>
 </template>
 <script>
