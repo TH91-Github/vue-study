3f0d4838ddd7d1d01b63f477b8be9e29ad9a9510
TH91-Github <je8035@ipartners.co.kr>
2023-01-27 10:49:06 +0900

    230127 netlify 배포 오류 테스트5

--- src/components/guide/GuideList.vue
+++ src/components/guide/GuideList.vue
@@ -18,8 +18,228 @@
                 </div>
               </li>
             </ul>
+            <p class="cm-guide__list-tit"><span class="cm-mark">#Color #컬러 #색상</span></p>
+            <ul class="cm-guide__list-wrap row">
+              <li class="item">
+                <div class="cm-guide__color">
+                  <button type="button" class="btn point-color1">
+                    <span class="codes">#3d1797</span>
+                  </button>
+                  <p class="tit">$point-color1</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__color">
+                  <button type="button" class="btn point-color2">
+                    <span class="codes">#1c7947</span>
+                  </button>
+                  <p class="tit">$point-color2</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__color">
+                  <button type="button" class="btn point-color3">
+                    <span class="codes">#f24c4c</span>
+                  </button>
+                  <p class="tit">$point-color3</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__color">
+                  <button type="button" class="btn point-color4">
+                    <span class="codes">#fee500</span>
+                  </button>
+                  <p class="tit">$point-color4</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__color">
+                  <button type="button" class="btn point-color5">
+                    <span class="codes">#6dcfdc</span>
+                  </button>
+                  <p class="tit">$point-color5</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__color">
+                  <button type="button" class="btn color-dark">
+                    <span class="codes">#262626</span>
+                  </button>
+                  <p class="tit">$color-dark</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__color">
+                  <button type="button" class="btn color-white">
+                    <span class="codes">#fff</span>
+                  </button>
+                  <p class="tit">$color-white</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__color">
+                  <button type="button" class="btn sub-color1">
+                    <span class="codes">#7f7f7f</span>
+                  </button>
+                  <p class="tit">$sub-color1</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__color">
+                  <button type="button" class="btn sub-color2">
+                    <span class="codes">#b4b4b4</span>
+                  </button>
+                  <p class="tit">$sub-color2</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__color">
+                  <button type="button" class="btn sub-dark">
+                    <span class="codes">#666</span>
+                  </button>
+                  <p class="tit">$sub-dark</p>
+                </div>
+              </li>
+            </ul>
+            <p class="cm-guide__list-tit"><span class="cm-mark">#ScreenSize #스크린 #사이즈</span></p>
+            <ul class="cm-guide__list-wrap row">
+              <li class="item">
+                <div class="cm-guide__screen">
+                  <button type="button" class="btn screen-w">
+                    <span class="value">768px;</span>
+                  </button>
+                  <p class="tit">$screen-s</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__screen">
+                  <button type="button" class="btn screen-w">
+                    <span class="value">1024px;</span>
+                  </button>
+                  <p class="tit">$screen-ms</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__screen">
+                  <button type="button" class="btn screen-w">
+                    <span class="value">1280px;</span>
+                  </button>
+                  <p class="tit">$screen-m</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__screen">
+                  <button type="button" class="btn screen-w">
+                    <span class="value">1680px;</span>
+                  </button>
+                  <p class="tit">$screen-l</p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__screen">
+                  <button type="button" class="btn screen-w">
+                    <span class="value">1920px;</span>
+                  </button>
+                  <p class="tit">$screen-xl</p>
+                </div>
+              </li>
+            </ul>
+            <p class="cm-guide__list-tit"><span class="cm-mark">#breakpoint #@media #반응형</span></p>
+            <ul class="cm-guide__list-wrap row">
+              <li class="item">
+                <div class="cm-guide__break">
+                  <button type="button" class="btn media-w">
+                    <span class="value">max-width:1920;</span>
+                  </button>
+                  <p class="tit">@include breakpoint(<span class="color-p3">pc</span>) {...} </p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__break">
+                  <button type="button" class="btn media-w">
+                    <span class="value">min-width:1680;</span>
+                  </button>
+                  <p class="tit">@include breakpoint(<span class="color-p3">wide</span>) {...} </p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__break">
+                  <button type="button" class="btn media-w">
+                    <span class="value">max-width:1280;</span>
+                  </button>
+                  <p class="tit">@include breakpoint(<span class="color-p3">tablet</span>) {...} </p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__break">
+                  <button type="button" class="btn media-w">
+                    <span class="value">max-width:768;</span>
+                  </button>
+                  <p class="tit">@include breakpoint(<span class="color-p3">mobile</span>) {...} </p>
+                </div>
+              </li>
+              <li class="item">
+                <div class="cm-guide__break">
+                  <button type="button" class="btn media-w">
+                    <span class="value">min: width, max: width</span>
+                  </button>
+                  <p class="tit">@include breakMinMax(<span class="color-p3">min-w, max-w</span>) {...} </p>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
+        <div class="cm-guide__template-cont">
+          <p class="cm-guide__template-tit">Common - 공통</p>
+          <div class="cm-guide__list">
+            <p class="cm-guide__list-tit"><span class="cm-mark">#Btn #버튼 #Button</span></p>
+            <ul class="cm-guide__list-wrap">
+              <li class="cm-guide__list-link">
+                <router-link
+                  to="/guide/CommonGuide"
+                  >
+                  /components/guide/CommonGuide
+                </router-link>
+              </li>
+            </ul>
+            <p class="cm-guide__list-tit"><span class="cm-mark">#Slider(Swiper) #슬라이드</span></p>
+            <ul class="cm-guide__list-wrap">
+              <li class="cm-guide__list-link">
+                <router-link
+                  to="/guide/SlideGuide"
+                  >
+                  /components/guide/SlideGuide
+                </router-link>
+              </li>
+            </ul>
+            <p class="cm-guide__list-tit"><span class="cm-mark">#Tab #탭 #탭메뉴</span></p>
+            <ul class="cm-guide__list-wrap">
+              <li class="cm-guide__list-link">
+                <router-link
+                  to="/guide/TabGuide"
+                  >
+                  /components/guide/TabGuide
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="cm-guide__template-cont">
+          <p class="cm-guide__template-tit">Component - 컴포넌트</p>
+          <div class="cm-guide__list">
+            <p class="cm-guide__list-tit"><span class="cm-mark">#Banner #배너 #띠배너</span></p>
+            <ul class="cm-guide__list-wrap">
+              <li class="cm-guide__list-link">
+                <router-link
+                  to="/guide/BannerGuide"
+                  >
+                  /components/guide/BannerGuide
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
